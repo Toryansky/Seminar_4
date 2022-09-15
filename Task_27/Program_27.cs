5d70ab9500995269969c63894d6482bb48467322
Denis Toryansky <dtoryansky@gmail.com>
2022-09-15 18:09:00 +0300

    Task 27 was added

---
+++ Task_27/Program_27.cs
@@ -0,0 +1,26 @@
+﻿// Напишите программу, которая принимает 
+// на вход число и выдаёт сумму цифр в числе.
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Введите число: ");
+        int number = Convert.ToInt32(Console.ReadLine());
+
+        int SummaDigits(int num)
+        {
+            int digitsquantity = Convert.ToString(number).Length;
+            int summa = 0;
+
+            for (int step = 0; step < digitsquantity; step++)
+            {
+                summa = summa + number % 10;
+                number = number / 10;
+            }
+            return summa;
+        }
+        int result = SummaDigits(number);
+        Console.WriteLine("Сумма цифр в числе: " + result);
+    }
+}
